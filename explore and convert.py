2d95df5dd9fbb27b7ee647d6c6f95e17ffcb99b6
Juico Bowley <68080175+Juicob@users.noreply.github.com>
2020-12-29 20:49:08 -0500

    iterate through levels of folders

--- explore and convert.py
+++ explore and convert.py
@@ -33,14 +33,57 @@ for file in tqdm(files):
 
 
 # %%
-top_dir_path = r"D:\Python_Projects\flatiron\class-materials\capstone-data\music"
 top_dir_list = os.listdir(top_dir_path)
 
 # for folder in top_dir_list:
     # print(os.path.join(top_dir_path, folder))
 artist_folders = [os.path.join(top_dir_path, folder) for folder in top_dir_list]
-print(artist_folders)
+# print(artist_folders)
 for folder in artist_folders:
     print(os.listdir(folder))
 
-# ! Reorganize folder structure in regions > artists > albums > song files
+# ! Reorganize folder structure in regions > artists > albums > track files
+# %%
+top_dir_path = "D:/Python_Projects/flatiron/class-materials/capstone-data/music"
+train_path = os.path.join(top_dir_path, 'TRAIN')
+for region in os.listdir(train_path)[:1]:
+    print(region)
+    region_path = os.path.join(train_path, region)
+    # print(region_path)
+    for artist in tqdm(os.listdir(region_path)[2:]):
+        print(artist)
+        artist_path = os.path.join(region_path, artist)
+        print('\n',artist_path)
+        if os.path.isdir(artist_path):
+            for album in tqdm(os.listdir(artist_path)[:]):
+                album_path = os.path.join(artist_path, album)
+                print(region, artist, album)
+                if os.path.isdir(album_path):
+                    for track in tqdm(os.listdir(album_path)):
+                        track_path = os.path.join(album_path, track)
+                        # if track.endswith('.jpg'):
+                        #     os.remove(track_path)
+                        #     print(f'REMOVED {track}')
+                        # if track.endswith('.png'):
+                        #     os.remove(track_path)
+                        #     print(f'REMOVED {track}')
+                        if track.endswith('.mp3'):
+                            # print(track)
+                            print(track_path)
+                            signal, sr = librosa.load(track_path, sr = 22050)
+                            y, sr = librosa.load(track_path, sr=32000, mono=True)
+                            melspec = librosa.feature.melspectrogram(y, sr=sr, n_mels = 128)
+                            melspec = librosa.power_to_db(melspec).astype(np.float32)
+
+                            fig = plt.Figure()
+                            # canvas = FigureCanvas(fig)
+                            ax = fig.add_subplot(111)
+                            img = librosa.display.specshow(melspec, ax=ax, sr=sr, fmax=16000, cmap='bone') # x_axis='time', y_axis='mel'
+                            fig.savefig(track_path.replace(".mp3",".png"))
+                # ! remove file if .jpg
+
+
+
+
+        # print(region_path)
+# %%
